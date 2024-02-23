47ad6b0042c33bd2066c88d75b1419d1f7297b28
Crowdbotics <team@crowdbotics.com>
2024-02-23 03:26:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gyan-kids-book-stor-46597",
+  url: "https://gyan-kids-book-stor-46597.botics.co",
+  api: "https://gyan-kids-book-stor-46597.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
